ceca314aaf06124e164bc13bcd3847b6adb32bdf
Somayyeh <Somayyeh.ghassemipour@gmail.com>
2022-01-12 11:57:08 +0100

    Added Wrapper component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Wrapper from './components/Wrapper';
 function App() {
   return (
     <div className="App">
+      <Wrapper>
+        
+      </Wrapper>
     </div>
   );
 }
