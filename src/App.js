160dedcc413df87ddbb35ecc727e4fa8a2ee23ac
Somayyeh <Somayyeh.ghassemipour@gmail.com>
2022-01-12 14:46:09 +0100

    Added Button component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import './App.css';
 import Wrapper from './components/Wrapper';
 import Screen from './components/Screen';
 import ButtonBox from './components/ButtonBox';
+import Button from './components/Button';
 
+const btnValues = [
+  ["C","+ -","%","+"],
+  ["1","2","3","-"],
+  ["4","5","6","x"],
+  ["7","8","9","/"],
+  ["0",".","="]
+];
 function App() {
   return (
     <div className="App">
       <Wrapper>
         <Screen value="0"/>
         <ButtonBox>
+        {
+            btnValues.flat().map((btn, i) => {
+              return (
+                <Button
+                key={i}
+                className={(`btn${i}`)}
+                value={btn}
+                onClick={() => { console.log(`${btn} clicked!`) }}
+                />
+              )
+            })
+          }
         </ButtonBox>
       </Wrapper>
     </div>
